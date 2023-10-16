b7548f6c50f6306c447b933e5bc2328289cb8f69
kunho <didrjsgh11@naver.com>
2023-10-16 16:39:05 +0900

    ✨feat: API 응답 형식 Class 작성

---
+++ backend/src/main/java/com/mogun/backend/controller/ApiResponse.java
@@ -0,0 +1,20 @@
+package com.mogun.backend.controller;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ApiResponse<T> {
+
+    private int code;
+    private HttpStatus status;
+    private String message;
+    private T data;
+
+    public ApiResponse(HttpStatus status, String message, T data) {
+        this.code = status.value();
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+}
