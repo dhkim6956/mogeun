5cee84dc9e08da858ece20fc20d25750591626b9
kunho <didrjsgh11@naver.com>
2023-10-20 10:33:21 +0900

    📌refactor: Project 파일 재생성

--- backend/src/main/java/com/mogun/backend/domain/gender/Repository/GenderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mogun.backend.domain.gender.Repository;
-
-import com.mogun.backend.domain.gender.Gender;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenderRepository extends JpaRepository<Gender, Integer> {
-
-    String findGenderNameById(int id);
-}
--- backend/src/main/java/com/mogun/backend/domain/userDetail/Repository/UserDetailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mogun.backend.domain.userDetail.Repository;
-
-import com.mogun.backend.domain.userDetail.UserDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDetailRepository extends JpaRepository<UserDetail, Integer> {
-
-}
