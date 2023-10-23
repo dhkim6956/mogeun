f957745961a20d89e784436728ea340bd9a15122
dhkim6956 <dhkim6956@naver.com>
2023-10-23 18:03:45 +0900

    Refactor: change package name & apply navigation

---
+++ Application/Mogeun/app/src/main/java/io/ssafy/mogeun/ui/screens/summary/SummaryScreen.kt
@@ -0,0 +1,19 @@
+package io.ssafy.mogeun.ui.screens.summary
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SummaryScreen() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(text = "Summary Screen")
+    }
+}
