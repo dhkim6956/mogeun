65b932a4561dec39fd04de03eedcc214a55591a5
kunho <didrjsgh11@naver.com>
2023-10-24 17:43:53 +0900

    ✨feat: User 신장 변화 로그 기능 추가

--- backend/src/main/java/com/mogun/backend/domain/userLog/userHeightLog/UserHeightLog.java
+++ backend/src/main/java/com/mogun/backend/domain/userLog/userHeightLog/UserHeightLog.java
@@ -1,6 +1,7 @@
 package com.mogun.backend.domain.userLog.userHeightLog;
 
 import com.mogun.backend.domain.user.User;
+import lombok.Builder;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 public class UserHeightLog {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "log_key")
     private Long logKey;
 
@@ -24,4 +26,13 @@ public class UserHeightLog {
 
     @Column(name = "changed_time")
     private LocalDateTime changedTime;
+
+    @Builder
+    public UserHeightLog(Long logKey, User user, float heightBefore, float heightAfter) {
+        this.logKey = logKey;
+        this.user = user;
+        this.heightBefore = heightBefore;
+        this.heightAfter = heightAfter;
+        this.changedTime = LocalDateTime.now();
+    }
 }
