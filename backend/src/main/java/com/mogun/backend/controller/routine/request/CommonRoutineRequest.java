762151f960bd88bd37941a8ffc5466a01a152ca0
kunho <didrjsgh11@naver.com>
2023-10-25 13:37:39 +0900

    ✨feat: 회원 별 전체 Routine 조회 기능

---
+++ backend/src/main/java/com/mogun/backend/controller/routine/request/CommonRoutineRequest.java
@@ -0,0 +1,29 @@
+package com.mogun.backend.controller.routine.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class CommonRoutineRequest {
+
+    @JsonProperty("user_email")
+    public String email;
+
+    @JsonProperty("routine_name")
+    public String routineName;
+
+    @JsonProperty("routine_key")
+    public int routineKey;
+
+    @JsonProperty("exec_key")
+    public int execKey;
+
+    @JsonProperty("total_sets")
+    public int sets;
+
+
+}
