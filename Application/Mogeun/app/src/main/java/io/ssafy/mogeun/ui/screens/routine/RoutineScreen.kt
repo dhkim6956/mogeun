115bb3bc7af6c50c7bb73108d8d7879019bb3cff
김세훈 <tpehd1009@gmail.com>
2023-10-26 12:12:54 +0900

    Style: 루틴 추가 페이지 UI 제작2

--- Application/Mogeun/app/src/main/java/io/ssafy/mogeun/ui/screens/routine/RoutineScreen.kt
+++ Application/Mogeun/app/src/main/java/io/ssafy/mogeun/ui/screens/routine/RoutineScreen.kt
@@ -3,16 +3,14 @@ package io.ssafy.mogeun.ui.screens.routine
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.navigation.NavHostController
 
 @Composable
-fun RoutineScreen(navController: NavHostController) {
+fun RoutineScreen() {
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
@@ -20,12 +18,5 @@ fun RoutineScreen(navController: NavHostController) {
             .background(color = Color.Green)
     ) {
         Text(text = "Routine Screen")
-
-        Button(
-            onClick = { navController.navigate("addroutine")},
-
-            ) {
-            Text(text = "test")
-        }
     }
 }
