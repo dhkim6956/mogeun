3552a66de24b33cd42e3f5b5c0133f1b447fce73
HanSeakHyeonㅎㅎㅁㅎㅁㄴㅇasdgasdfasdfㅎ <togkstls1008@naver.com>
2023-11-02 03:21:33 +0900

    ✨feat: body component 입력 1. Datee Pickeer 추가

--- Application/Mogeun/app/src/main/java/io/ssafy/mogeun/ui/screens/routine/execution/ExecutionScreen.kt
+++ Application/Mogeun/app/src/main/java/io/ssafy/mogeun/ui/screens/routine/execution/ExecutionScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,6 +22,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -28,9 +30,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableLongStateOf
@@ -39,13 +43,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import java.util.Calendar
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -59,6 +66,15 @@ fun ExecutionScreen() {
             .background(color = Color.Yellow)
     ) {
         ExerciseEMGScreen()
+        val chosenYear = remember { mutableStateOf(currentYear) }
+        val chosenMonth = remember { mutableStateOf(currentMonth) }
+        val chosenDay = remember { mutableStateOf(currentDay) }
+
+        DateSelectionSection(
+            onYearChosen = { chosenYear.value = it.toInt() },
+            onMonthChosen = { chosenMonth.value = monthsNames.indexOf(it) },
+            onDayChosen = { chosenDay.value = it.toInt() },
+        )
 
     //HorizontalPager(state = state, modifier = Modifier.fillMaxSize()) {page ->
     //            Box(
@@ -233,6 +249,84 @@ private fun ScrollableTabRow(
 
 //private fun ScrollButton(){}
 
+//--------------------------------------------
+
+@Composable
+fun DateSelectionSection(
+    onYearChosen: (String) -> Unit,
+    onMonthChosen: (String) -> Unit,
+    onDayChosen: (String) -> Unit,
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(120.dp)
+    ) {
+        InfiniteItemsPicker(
+            items = days,
+            firstIndex = Int.MAX_VALUE / 2 + (currentDay - 2),
+            onItemSelected =  onDayChosen
+        )
+
+        InfiniteItemsPicker(
+            items = monthsNames,
+            firstIndex = Int.MAX_VALUE / 2 - 4 + currentMonth,
+            onItemSelected =  onMonthChosen
+        )
+
+        InfiniteItemsPicker(
+            items = years,
+            firstIndex = Int.MAX_VALUE / 2 + (currentYear - 1967),
+            onItemSelected = onYearChosen
+        )
+    }
+}
+
+@Composable
+fun InfiniteItemsPicker(
+    modifier: Modifier = Modifier,
+    items: List<String>,
+    firstIndex: Int,
+    onItemSelected: (String) -> Unit,
+) {
+
+    val listState = rememberLazyListState(firstIndex)
+    val currentValue = remember { mutableStateOf("") }
+
+    LaunchedEffect(key1 = !listState.isScrollInProgress) {
+        onItemSelected(currentValue.value)
+        listState.animateScrollToItem(index = listState.firstVisibleItemIndex)
+    }
+
+    Box(modifier = Modifier.height(106.dp)) {
+        LazyColumn(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            state = listState,
+            content = {
+                items(count = Int.MAX_VALUE, itemContent = {
+                    val index = it % items.size
+                    if (it == listState.firstVisibleItemIndex + 1) {
+                        currentValue.value = items[index]
+                    }
+
+                    Spacer(modifier = Modifier.height(6.dp))
+
+                    Text(
+                        text = items[index],
+                        modifier = Modifier.alpha(if (it == listState.firstVisibleItemIndex + 1) 1f else 0.3f),
+                        style = MaterialTheme.typography.bodyMedium,
+                        textAlign = TextAlign.Center
+                    )
+
+                    Spacer(modifier = Modifier.height(6.dp))
+                })
+            }
+        )
+    }
+}
+//--------------------------------------------
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewEMGScreen(){
@@ -240,3 +334,26 @@ fun PreviewEMGScreen(){
 
 
 }
+
+
+val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+val currentDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+val currentMonth = Calendar.getInstance().get(Calendar.MONTH)
+
+val years = (1950..2050).map { it.toString() }
+val monthsNumber = (1..12).map { it.toString() }
+val days = (1..31).map { it.toString() }
+val monthsNames = listOf(
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+)
