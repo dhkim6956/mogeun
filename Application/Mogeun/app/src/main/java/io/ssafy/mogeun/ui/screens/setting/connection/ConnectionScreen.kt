fc244c8727d354c0e85a1a8b9878590ecc59e754
dhkim6956 <dhkim6956@naver.com>
2023-11-03 18:08:24 +0900

    ✨feat: ble initialized

--- Application/Mogeun/app/src/main/java/io/ssafy/mogeun/ui/screens/setting/connection/ConnectionScreen.kt
+++ Application/Mogeun/app/src/main/java/io/ssafy/mogeun/ui/screens/setting/connection/ConnectionScreen.kt
@@ -1,5 +1,14 @@
 package io.ssafy.mogeun.ui.screens.setting.connection
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
+import android.content.Context
+import android.content.pm.PackageManager
 import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -13,8 +22,10 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.ssafy.mogeun.ui.AppViewModelProvider
 import io.ssafy.mogeun.ui.theme.MogeunTheme
@@ -23,7 +34,33 @@ import kotlinx.coroutines.launch
 @Composable
 fun ConnectionScreen(viewModel: ConnectionViewModel = viewModel(factory = AppViewModelProvider.Factory)) {
 
+    Log.d("Connection", "확인용 로그")
+
     val coroutineScope = rememberCoroutineScope()
+
+    val context = LocalContext.current
+    val localActivity = context as Activity
+    val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+    val bluetoothAdapter = bluetoothManager.adapter
+    val bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+
+    val scanSettings = ScanSettings.Builder()
+        .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
+        .build()
+
+    val permissions = arrayOf(Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT)
+
+    requestPermissions(context, localActivity, permissions)
+
+    for (permission in permissions) {
+        if(ActivityCompat.checkSelfPermission( context, permission) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(localActivity, permissions, 0)
+        }
+    }
+
+    bluetoothLeScanner.startScan(null, scanSettings, scanCallback)
+
+
 
     val emgLastData by viewModel.emgStream.collectAsState()
     val emgInput = viewModel.emgInput.value
@@ -61,10 +98,36 @@ fun ConnectionScreen(viewModel: ConnectionViewModel = viewModel(factory = AppVie
     }
 }
 
+val scanCallback: ScanCallback = object : ScanCallback() {
+    @SuppressLint("MissingPermission")
+    override fun onScanResult(callbackType: Int, result: ScanResult?) {
+        Log.d("Connection", "run")
+        Log.d("Connection", result.toString())
+
+        if(result?.device?.name != null) {
+            var uuid = "null"
+
+            Log.d("Connection", result.scanRecord!!.serviceUuids.toString())
+
+            if(result.scanRecord?.serviceUuids != null) {
+                uuid = result.scanRecord!!.serviceUuids.toString()
+            }
+        }
+    }
+}
+
+fun requestPermissions(context: Context, localActivity: Activity, permissions: Array<String>)  {
+    for(permission in permissions) {
+        if(ActivityCompat.checkSelfPermission( context, permission) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(localActivity, permissions, 0)
+        }
+    }
+}
+
 
 @Preview
 @Composable
-fun TestScreenPreview() {
+fun ConnectionScreenPreview() {
     MogeunTheme {
         ConnectionScreen()
     }
