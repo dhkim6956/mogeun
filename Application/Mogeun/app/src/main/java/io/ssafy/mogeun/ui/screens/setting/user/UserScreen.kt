4c84a1bfa7245aa258a4fbda4f84b6931cfc0c19
이민규 <lmg386411@gmail.com>
2023-11-07 17:42:03 +0900

    📌refactor: 회원정보 변경 키보드 옵션 추가

--- Application/Mogeun/app/src/main/java/io/ssafy/mogeun/ui/screens/setting/user/UserScreen.kt
+++ Application/Mogeun/app/src/main/java/io/ssafy/mogeun/ui/screens/setting/user/UserScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -17,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -57,7 +59,14 @@ fun UserScreen(
                     } else {
                         viewModel.updateNickname(it)
                     }
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(onDone = {
+                    keyboardController?.hide()
                 })
+            )
             Text(text = "신장")
             TextField(
                 value = heightText,
@@ -70,8 +79,12 @@ fun UserScreen(
                     }
                 },
                 keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Number
-                )
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(onDone = {
+                    keyboardController?.hide()
+                })
             )
             Text(text = "체중")
             TextField(
@@ -85,8 +98,12 @@ fun UserScreen(
                     }
                 },
                 keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Number
-                )
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(onDone = {
+                    keyboardController?.hide()
+                })
             )
             Text(text = "골격근량")
             TextField(
@@ -100,8 +117,12 @@ fun UserScreen(
                     }
                 },
                 keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Number
-                )
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(onDone = {
+                    keyboardController?.hide()
+                })
             )
             Text(text = "체지방량")
             TextField(
@@ -115,8 +136,12 @@ fun UserScreen(
                     }
                 },
                 keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Number
-                )
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(onDone = {
+                    keyboardController?.hide()
+                })
             )
             Button(onClick = {
                 viewModel.updateUser()
