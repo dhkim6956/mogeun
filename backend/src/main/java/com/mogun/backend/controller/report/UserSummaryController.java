12c85615f51415e9cfb5e1b0cdb139125669700b
kunho <didrjsgh11@naver.com>
2023-11-12 17:49:49 +0900

    ✨feat: 최다 수행 운동 및 최대 무게 운동 조회 기능 추가

--- backend/src/main/java/com/mogun/backend/controller/report/UserSummaryController.java
+++ backend/src/main/java/com/mogun/backend/controller/report/UserSummaryController.java
@@ -4,6 +4,9 @@ import com.mogun.backend.ApiResponse;
 import com.mogun.backend.domain.report.setReport.SetReport;
 import com.mogun.backend.service.ServiceStatus;
 import com.mogun.backend.service.report.SetReportService;
+import com.mogun.backend.service.report.dto.MostPerformedDto;
+import com.mogun.backend.service.report.dto.MostWeightDto;
+import com.mogun.backend.service.report.dto.RoutineReportDto;
 import com.mogun.backend.service.userLog.UserLogService;
 import com.mogun.backend.service.userLog.dto.UserLogDto;
 import com.mogun.backend.service.userLog.dto.UserMuscleAndFatLogDto;
@@ -14,14 +17,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/API/Summary")
 public class UserSummaryController {
 
-    private UserLogService logService;
-    private SetReportService setReportService;
+    private final UserLogService logService;
+    private final SetReportService setReportService;
 
     @GetMapping("/LastLogs")
     public ApiResponse<Object> getLast10BodyFatAndMuscleMassLogs(@RequestParam("user_key") int userKey) {
@@ -29,6 +35,30 @@ public class UserSummaryController {
         ServiceStatus<UserMuscleAndFatLogDto> result = logService.getLast10Logs(UserLogDto.builder()
                 .userKey(userKey)
                 .build());
+
+        if(result.getStatus() != 100)
+            return ApiResponse.badRequest(result.getMessage());
+
+        return ApiResponse.ok(result.getData());
+    }
+
+    @GetMapping("/ExerciseMost")
+    public ApiResponse<Object> getMostExercised(@RequestParam("user_key")int userKey, @RequestParam("search_type")int option) {
+
+        ServiceStatus<MostPerformedDto> result = setReportService.mostPerformedExercise(RoutineReportDto.builder()
+                .userKey(userKey).build(), option);
+
+        if(result.getStatus() != 100)
+            return ApiResponse.badRequest(result.getMessage());
+
+        return ApiResponse.ok(result.getData());
+    }
+
+    @GetMapping("/ExerciseWeight")
+    public ApiResponse<Object> getMostWeighted(@RequestParam("user_key")int userKey, @RequestParam("search_type")int option) {
+
+        ServiceStatus<MostWeightDto> result = setReportService.mostWeightedExercise(RoutineReportDto.builder()
+                .userKey(userKey).build(), option);
 
         if(result.getStatus() != 100)
             return ApiResponse.badRequest(result.getMessage());
--- backend/src/main/java/com/mogun/backend/service/report/SetReportService.java
+++ backend/src/main/java/com/mogun/backend/service/report/SetReportService.java
@@ -1,16 +1,26 @@
 package com.mogun.backend.service.report;
 
+import com.mogun.backend.domain.exercise.Exercise;
+import com.mogun.backend.domain.exercise.repository.ExerciseRepository;
 import com.mogun.backend.domain.report.routineReport.RoutineReport;
 import com.mogun.backend.domain.report.routineReport.repository.RoutineReportRepository;
+import com.mogun.backend.domain.report.setReport.repository.ExecCountInterface;
+import com.mogun.backend.domain.report.setReport.repository.ExecWeightInterface;
 import com.mogun.backend.domain.report.setReport.repository.SetReportRepository;
 import com.mogun.backend.domain.routine.userRoutinePlan.UserRoutinePlan;
 import com.mogun.backend.domain.routine.userRoutinePlan.repository.UserRoutinePlanRepository;
+import com.mogun.backend.domain.user.User;
+import com.mogun.backend.domain.user.repository.UserRepository;
 import com.mogun.backend.service.ServiceStatus;
+import com.mogun.backend.service.report.dto.MostPerformedDto;
+import com.mogun.backend.service.report.dto.MostWeightDto;
 import com.mogun.backend.service.report.dto.RoutineReportDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,6 +31,8 @@ public class SetReportService {
     private final SetReportRepository setReportRepository;
     private final RoutineReportRepository routineReportRepository;
     private final UserRoutinePlanRepository planRepository;
+    private final UserRepository userRepository;
+    private final ExerciseRepository exerciseRepository;
 
     public ServiceStatus<Object> insertSetReport(RoutineReportDto dto) {
 
@@ -38,5 +50,67 @@ public class SetReportService {
         setReportRepository.save(dto.toSetReportEntity(report.get(), plan.get().getExercise()));
 
         return ServiceStatus.okStatus();
+    }
+
+    public ServiceStatus<MostPerformedDto> mostPerformedExercise(RoutineReportDto dto, int option) {
+
+        Optional<User> user = userRepository.findById(dto.getUserKey());
+        if(user.isEmpty())
+            return ServiceStatus.errorStatus("요청 오류: 등록되지 않은 회원");
+
+        LocalDateTime startDate;
+        LocalDateTime now = LocalDateTime.now();
+        if(option == 1)
+            startDate = LocalDateTime.of(2000, 1, 1, 0, 0, 0);
+        else if(option == 2)
+            startDate = LocalDateTime.of(now.getYear(), 1, 1, 0, 0, 0);
+        else if(option == 3)
+            startDate = LocalDateTime.of(now.getYear(), now.getMonth(), 1, 0, 0, 0);
+        else
+            return ServiceStatus.errorStatus("요청 오류: 올바르지 않은 날짜 옵션");
+
+        List<ExecCountInterface> countDtoList = setReportRepository.findRangedExerciseByUserAndStartDate(user.get(), startDate);
+        Optional<Exercise> exec = exerciseRepository.findById(countDtoList.get(0).getExec_Key());
+
+        return ServiceStatus.<MostPerformedDto>builder()
+                .status(100)
+                .message("SUCCESS")
+                .data(MostPerformedDto.builder()
+                        .execName(exec.get().getName())
+                        .imagePath(exec.get().getImagePath())
+                        .performCount(countDtoList.get(0).getExec_Count())
+                        .build())
+                .build();
+    }
+
+    public ServiceStatus<MostWeightDto> mostWeightedExercise(RoutineReportDto dto, int option) {
+
+        Optional<User> user = userRepository.findById(dto.getUserKey());
+        if(user.isEmpty())
+            return ServiceStatus.errorStatus("요청 오류: 등록되지 않은 회원");
+
+        LocalDateTime startDate;
+        LocalDateTime now = LocalDateTime.now();
+        if(option == 1)
+            startDate = LocalDateTime.of(2000, 1, 1, 0, 0, 0);
+        else if(option == 2)
+            startDate = LocalDateTime.of(now.getYear(), 1, 1, 0, 0, 0);
+        else if(option == 3)
+            startDate = LocalDateTime.of(now.getYear(), now.getMonth(), 1, 0, 0, 0);
+        else
+            return ServiceStatus.errorStatus("요청 오류: 올바르지 않은 날짜 옵션");
+
+        List<ExecWeightInterface> weightDtoList = setReportRepository.findRangedWeightByUserAndStartDate(user.get(), startDate);
+        Optional<Exercise> exec = exerciseRepository.findById(weightDtoList.get(0).getExec_Key());
+
+        return ServiceStatus.<MostWeightDto>builder()
+                .status(100)
+                .message("SUCCESS")
+                .data(MostWeightDto.builder()
+                        .execName(exec.get().getName())
+                        .imagePath(exec.get().getImagePath())
+                        .weight(weightDtoList.get(0).getTrain_weight())
+                        .build())
+                .build();
     }
 }
