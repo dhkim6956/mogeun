35a3323568e90df53f21143ca13a8b18ca9c4d99
kunho <didrjsgh11@naver.com>
2023-11-20 10:29:28 +0900

    📌refactor: Spring Boot의 기준 시간을 Asia/Seoul로 고정

--- backend/src/main/java/com/mogun/backend/MainApplication.java
+++ backend/src/main/java/com/mogun/backend/MainApplication.java
@@ -3,12 +3,14 @@ package com.mogun.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
 import java.time.ZoneId;
 import java.util.TimeZone;
 
 @SpringBootApplication
 public class MainApplication {
 
+	@PostConstruct
 	public void started() {
 
 		TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Asia/Seoul")));
