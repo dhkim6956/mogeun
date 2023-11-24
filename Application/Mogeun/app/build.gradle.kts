1764a94057cf0637b24da368227f97a23485f47d
dhkim6956 <dhkim6956@naver.com>
2023-11-24 11:28:42 +0900

    🌈style: change app icon and wear app name

--- Application/Mogeun/app/build.gradle.kts
+++ Application/Mogeun/app/build.gradle.kts
@@ -117,7 +117,7 @@ dependencies {
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     // Kotlin serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.1")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("io.coil-kt:coil-compose:2.5.0")
