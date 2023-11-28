ce467f5c70985a5733da4f972c099d39c75a8073
dhkim6956 <dhkim6956@naver.com>
2023-11-28 22:33:55 +0900

    ✨feat: update version code

--- Application/Mogeun/watch/build.gradle.kts
+++ Application/Mogeun/watch/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "io.ssafy.mogeun"
         minSdk = 30
         targetSdk = 33
-        versionCode = 10
-        versionName = "1.0.1"
+        versionCode = 11
+        versionName = "1.1.0"
         vectorDrawables {
             useSupportLibrary = true
         }
